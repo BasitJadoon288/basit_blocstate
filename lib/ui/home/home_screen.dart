4d7c53f1f32898b1a23961123372452014c914aa
bi.it-025 <bi.it-025@munico-group.local>
2025-02-22 09:17:53 +0500

    cart page

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import '../cart/cart_screen.dart';
-
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,14 +20,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<Product> products = [
     Product(name: 'Product 1', price: 29.99, imageUrl: 'https://static.vecteezy.com/system/resources/thumbnails/025/181/412/small/picture-a-captivating-scene-of-a-tranquil-lake-at-sunset-ai-generative-photo.jpg', rating: 4.5, discount: 10),
     Product(name: 'Product 2', price: 49.99, imageUrl: 'https://static.vecteezy.com/system/resources/thumbnails/039/545/117/small/ai-generated-beautiful-rose-flowers-with-butterflies-flying-on-them-video.jpg', rating: 4.0, discount: 5),
     Product(name: 'Product 3', price: 19.99, imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvsCKzzYjPgyGodDPPWHKo4FoXMriKDLHK6Jz359BFpkc8l03dY5Zt8gpGM7ZeY2kquI0&usqp=CAU', rating: 3.5, discount: 0),
     Product(name: 'Product 4', price: 99.99, imageUrl: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/01/bd/a3/see-two-for-the-price.jpg?w=900&h=500&s=1', rating: 5.0, discount: 20),
     Product(name: 'Product 5', price: 59.99, imageUrl: 'https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg', rating: 4.2, discount: 15),
+    Product(name: 'Product 6', price: 29.99, imageUrl: 'https://static.vecteezy.com/system/resources/thumbnails/025/181/412/small/picture-a-captivating-scene-of-a-tranquil-lake-at-sunset-ai-generative-photo.jpg', rating: 4.5, discount: 10),
+    Product(name: 'Product 7', price: 49.99, imageUrl: 'https://static.vecteezy.com/system/resources/thumbnails/039/545/117/small/ai-generated-beautiful-rose-flowers-with-butterflies-flying-on-them-video.jpg', rating: 4.0, discount: 5),
+    Product(name: 'Product 8', price: 19.99, imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvsCKzzYjPgyGodDPPWHKo4FoXMriKDLHK6Jz359BFpkc8l03dY5Zt8gpGM7ZeY2kquI0&usqp=CAU', rating: 3.5, discount: 0),
+    Product(name: 'Product 9', price: 99.99, imageUrl: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/01/bd/a3/see-two-for-the-price.jpg?w=900&h=500&s=1', rating: 5.0, discount: 20),
+    Product(name: 'Product 10', price: 59.99, imageUrl: 'https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg', rating: 4.2, discount: 15),
   ];
+
+  List<Product> cart = [];
 
   @override
   Widget build(BuildContext context) {
@@ -36,21 +48,25 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.deepPurple,
         title: const Row(
           children: [
-            Icon(Icons.storefront, size: 30, color: Colors.white,),
+            Icon(Icons.storefront, size: 30, color: Colors.white),
             SizedBox(width: 8),
             Text('Daraz Clone', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24, color: Colors.white)),
           ],
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.search, color: Colors.white,),
-            onPressed: () {
-            },
+            icon: const Icon(Icons.search, color: Colors.white),
+            onPressed: () {},
           ),
           IconButton(
-            icon: Icon(Icons.shopping_cart, color: Colors.white),
+            icon: const Icon(Icons.shopping_cart, color: Colors.white),
             onPressed: () {
-              Navigator.pop(context, MaterialPageRoute(builder: (context) => CartScreen()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CartScreen(cart: cart),
+                ),
+              );
             },
           ),
         ],
@@ -61,13 +77,23 @@ class HomePage extends StatelessWidget {
         child: GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
-            crossAxisSpacing: 16.0,
-            mainAxisSpacing: 16.0,
+            crossAxisSpacing: 12.0,
+            mainAxisSpacing: 12.0,
             childAspectRatio: 0.75,
           ),
           itemCount: products.length,
           itemBuilder: (context, index) {
-            return _ProductCard(product: products[index]);
+            return _ProductCard(
+              product: products[index],
+              onAddToCart: () {
+                setState(() {
+                  cart.add(products[index]);
+                });
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('${products[index].name} added to cart')),
+                );
+              },
+            );
           },
         ),
       ),
@@ -87,14 +113,21 @@ class Product {
 
 class _ProductCard extends StatelessWidget {
   final Product product;
+  final VoidCallback onAddToCart;
 
-  const _ProductCard({required this.product});
+  const _ProductCard({required this.product, required this.onAddToCart});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        // Implement product detail navigation
+        // Navigate to product detail screen
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProductDetailScreen(product: product),
+          ),
+        );
       },
       child: Card(
         elevation: 10,
@@ -123,51 +156,6 @@ class _ProductCard extends StatelessWidget {
                         fit: BoxFit.cover,
                       ),
                     ),
-                    child: Stack(
-                      children: [
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: product.discount > 0
-                              ? Container(
-                            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                            decoration: const BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(12),
-                                topRight: Radius.circular(12),
-                              ),
-                            ),
-                            child: Text(
-                              '-${product.discount}%',
-                              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                            ),
-                          )
-                              : SizedBox(),
-                        ),
-                        Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              product.name,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                color: Colors.white,
-                                shadows: [
-                                  Shadow(
-                                    offset: Offset(2, 2),
-                                    blurRadius: 4,
-                                    color: Colors.black.withOpacity(0.6),
-                                  ),
-                                ],
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
                   ),
                 ),
                 Padding(
@@ -175,7 +163,7 @@ class _ProductCard extends StatelessWidget {
                   child: Row(
                     children: [
                       const Icon(Icons.star, color: Colors.yellow, size: 16),
-                      Text(' ${product.rating}', style: TextStyle(fontSize: 14, color: Colors.white)),
+                      Text(' ${product.rating}', style: const TextStyle(fontSize: 14, color: Colors.white)),
                     ],
                   ),
                 ),
@@ -192,22 +180,17 @@ class _ProductCard extends StatelessWidget {
                 ),
                 Padding(
                   padding: const EdgeInsets.only(bottom: 8, left: 8),
-                  child: Container(
-                    width: 120, // Adjust width for a better fit
-                    height: 40, // Adjust height for better proportions
+                  child: SizedBox(
+                    width: 120,
+                    height: 40,
                     child: ElevatedButton(
-                      onPressed: () {
-                        // Simulate adding to cart
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('${product.name} added to cart', style: TextStyle(fontSize: 12))),
-                        );
-                      },
+                      onPressed: onAddToCart,
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.white,
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        padding: EdgeInsets.symmetric(vertical: 12),
+                        padding: const EdgeInsets.symmetric(vertical: 12),
                       ),
                       child: const Text(
                         'Add to Cart',
@@ -219,6 +202,242 @@ class _ProductCard extends StatelessWidget {
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductDetailScreen extends StatelessWidget {
+  final Product product;
+
+  const ProductDetailScreen({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    double discountedPrice = product.price - (product.price * product.discount / 100);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.name, style: const TextStyle(color: Colors.white)),
+        backgroundColor: Colors.deepPurple,
+        elevation: 4,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Image.network(
+                product.imageUrl,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              product.name,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                const Icon(Icons.star, color: Colors.yellow, size: 18),
+                Text(
+                  ' ${product.rating}',
+                  style: const TextStyle(fontSize: 16, color: Colors.black),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            if (product.discount > 0) ...[
+              Text(
+                'Discount: ${product.discount}%',
+                style: const TextStyle(fontSize: 16, color: Colors.red),
+              ),
+              const SizedBox(height: 8),
+            ],
+            Text(
+              'Original Price: \$${product.price.toStringAsFixed(2)}',
+              style: const TextStyle(fontSize: 18, color: Colors.black54, decoration: TextDecoration.lineThrough),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Price after Discount: \$${discountedPrice.toStringAsFixed(2)}',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.deepPurple),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Description:',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'This is a placeholder description of the product. It can include details such as features, materials, dimensions, and usage instructions.',
+              style: const TextStyle(fontSize: 16, color: Colors.black87),
+            ),
+            const SizedBox(height: 16),
+            // Center(
+            //   child: ElevatedButton(
+            //     onPressed: () {
+            //       // Add to cart functionality can go here.
+            //     },
+            //     style: ElevatedButton.styleFrom(
+            //       backgroundColor: Colors.deepPurple,
+            //       padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 40),
+            //       shape: RoundedRectangleBorder(
+            //         borderRadius: BorderRadius.circular(12),
+            //       ),
+            //     ),
+            //     child: const Text('Add to Cart', style: TextStyle(fontSize: 18, color: Colors.white)),
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class CartScreen extends StatelessWidget {
+  final List<Product> cart;
+
+  const CartScreen({super.key, required this.cart});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Cart', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.deepPurple,
+        elevation: 4,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: cart.length,
+                itemBuilder: (context, index) {
+                  final product = cart[index];
+                  return ListTile(
+                    contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                    leading: Image.network(product.imageUrl, width: 50, fit: BoxFit.cover),
+                    title: Text(product.name),
+                    subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
+                    trailing: const Icon(Icons.delete, color: Colors.red),
+                  );
+                },
+              ),
+            ),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CheckoutScreen(cartProducts: cart),
+            ),
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.deepPurple, // Change this to your desired color
+        ),
+      child: const Text('Proceed to Checkout', style: TextStyle(color: Colors.white),),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CheckoutScreen extends StatefulWidget {
+  final List<Product> cartProducts;
+
+  const CheckoutScreen({super.key, required this.cartProducts});
+
+  @override
+  _CheckoutScreenState createState() => _CheckoutScreenState();
+}
+
+class _CheckoutScreenState extends State<CheckoutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double totalAmount = widget.cartProducts.fold(0, (sum, product) => sum + product.price);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Checkout', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.deepPurple,
+        elevation: 4,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Items in your cart:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemCount: widget.cartProducts.length,
+                itemBuilder: (context, index) {
+                  final product = widget.cartProducts[index];
+                  return ListTile(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                    leading: Image.network(product.imageUrl, width: 50, fit: BoxFit.cover),
+                    title: Text(product.name, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                    subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
+                    trailing: const Icon(Icons.delete, color: Colors.red),
+                    onTap: () {
+                      // Optionally, you can add a handler for a tap on the list tile.
+                    },
+                    onLongPress: () {
+                      // Remove the product from the cart when the delete icon is pressed
+                      setState(() {
+                        widget.cartProducts.removeAt(index); // Removes the product at the current index
+                      });
+                    },
+                  );
+                },
+              ),
+            ),
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                'Total: \$${totalAmount.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Order placed successfully!')),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: const Text('Proceed to Checkout', style: TextStyle(fontSize: 18, color: Colors.white)),
+              ),
+            ),
+          ],
         ),
       ),
     );
